8ed038e3bfb1b3b83e03e307c6e461bebc071256
Dillipsahoo <dillipsahooblr@gmail.com>
2025-09-28 20:48:16 +0530

    Add files via upload

---
+++ MathOperation.py
@@ -0,0 +1,12 @@
+a=input('Enter the First Number: ')
+b=input('Enter the Second Number: ')
+first_number = int(a)
+second_number = int(b)
+Addition = first_number + second_number
+Subtract = first_number - second_number
+Multiply = first_number * second_number
+Division = first_number / second_number
+print("Addition: ",Addition)
+print("Addition: ",Subtract)
+print("Addition: ",Multiply)
+print("Addition: ",Division)
---
+++ Name.py
@@ -0,0 +1,5 @@
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+
+print("Hello  " + fname + " " + lname + " Welcome to Python Program")
+
