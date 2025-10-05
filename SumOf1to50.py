f98ba0604ce14c1f67663c517c0319209c68624a
Dillipsahoo <dillipsahooblr@gmail.com>
2025-10-05 22:08:38 +0530

    Add files via upload

---
+++ SumOf1to50.py
@@ -0,0 +1,8 @@
+# Initialize a variable to store the sum
+totalsum = 0
+
+# Loop from 1 to 50
+for number in range(1, 51):
+    totalsum += number  # Add each number to the totalsum
+
+print("The sum of integers from 1 to 50 is:",totalsum)
